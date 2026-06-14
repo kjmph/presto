449aef7a3f0c6e237e916e040c33a60d2483fc9a
Kyle Hubert <kjmph@users.noreply.github.com>
2026-06-14 12:45:57 -0400

    Guard join-key filter pushdown by replicated source size
    PushJoinKeyFilterBelowAggregation can clone a selective filtering
    source below an aggregation. This helps when the source is small, but
    hurts when the replicated source is large across tasks.
    
    Pass TaskCountEstimator into the rule and require the estimated
    replicated filtering-source bytes to fit under
    join_max_broadcast_table_size before firing. Add coverage for a
    selective source that is too large to clone.

--- presto-main-base/src/test/java/com/facebook/presto/sql/planner/iterative/rule/TestPushJoinKeyFilterBelowAggregation.java
+++ presto-main-base/src/test/java/com/facebook/presto/sql/planner/iterative/rule/TestPushJoinKeyFilterBelowAggregation.java
@@ -14,6 +14,7 @@
 package com.facebook.presto.sql.planner.iterative.rule;
 
 import com.facebook.presto.cost.PlanNodeStatsEstimate;
+import com.facebook.presto.cost.TaskCountEstimator;
 import com.facebook.presto.spi.plan.EquiJoinClause;
 import com.facebook.presto.spi.plan.PlanNodeId;
 import com.facebook.presto.spi.relation.VariableReferenceExpression;
@@ -24,6 +25,7 @@ import org.testng.annotations.Test;
 
 import java.util.Optional;
 
+import static com.facebook.presto.SystemSessionProperties.JOIN_MAX_BROADCAST_TABLE_SIZE;
 import static com.facebook.presto.SystemSessionProperties.PUSH_AGGREGATION_THROUGH_JOIN;
 import static com.facebook.presto.common.type.BigintType.BIGINT;
 import static com.facebook.presto.common.type.DoubleType.DOUBLE;
@@ -44,7 +46,7 @@ public class TestPushJoinKeyFilterBelowAggregation
     @Test
     public void testPushesJoinKeyFilterBelowFilteredAggregation()
     {
-        tester().assertThat(new PushJoinKeyFilterBelowAggregation(getFunctionManager()))
+        tester().assertThat(new PushJoinKeyFilterBelowAggregation(getFunctionManager(), new TaskCountEstimator(() -> 4)))
                 .setSystemProperty(PUSH_AGGREGATION_THROUGH_JOIN, "true")
                 .on(p -> {
                     VariableReferenceExpression leftKey = p.variable("left_key", BIGINT);
@@ -88,5 +90,41 @@ public class TestPushJoinKeyFilterBelowAggregation
                                                 values("left_key", "quantity"),
                                                 values(ImmutableMap.of()))))),
                         values("right_key")));
+    }
+
+    @Test
+    public void testDoesNotPushWhenFilteringSourceIsTooLargeToClone()
+    {
+        tester().assertThat(new PushJoinKeyFilterBelowAggregation(getFunctionManager(), new TaskCountEstimator(() -> 4)))
+                .setSystemProperty(PUSH_AGGREGATION_THROUGH_JOIN, "true")
+                .setSystemProperty(JOIN_MAX_BROADCAST_TABLE_SIZE, "5kB")
+                .on(p -> {
+                    VariableReferenceExpression leftKey = p.variable("left_key", BIGINT);
+                    VariableReferenceExpression quantity = p.variable("quantity", DOUBLE);
+                    VariableReferenceExpression avgValue = p.variable("avg_value", DOUBLE);
+                    VariableReferenceExpression rightKey = p.variable("right_key", BIGINT);
+
+                    return p.join(
+                            INNER,
+                            p.filter(
+                                    p.rowExpression("avg_value IS NOT NULL"),
+                                    p.aggregation(aggregation -> aggregation
+                                            .source(p.values(new PlanNodeId("aggregation_source"), 100_000, leftKey, quantity))
+                                            .addAggregation(avgValue, p.rowExpression("avg(quantity)"))
+                                            .singleGroupingSet(leftKey))),
+                            p.values(new PlanNodeId("filtering_source"), 2_000, rightKey),
+                            ImmutableList.of(new EquiJoinClause(leftKey, rightKey)),
+                            ImmutableList.of(leftKey, avgValue, rightKey),
+                            Optional.empty(),
+                            Optional.empty(),
+                            Optional.empty());
+                })
+                .overrideStats("aggregation_source", PlanNodeStatsEstimate.builder()
+                        .setOutputRowCount(100_000)
+                        .build())
+                .overrideStats("filtering_source", PlanNodeStatsEstimate.builder()
+                        .setOutputRowCount(2_000)
+                        .build())
+                .doesNotFire();
     }
 }
